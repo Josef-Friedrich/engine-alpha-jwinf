2a1118b8884a4b00aca94b3837425d4b84a9ab55
Josef Friedrich <josef@friedrich.rocks>
2023-11-05 17:29:10 +0100

    Main menu

--- src/main/java/rocks/friedrich/jwinf/engine/scenes/MainMenu.java
+++ src/main/java/rocks/friedrich/jwinf/engine/scenes/MainMenu.java
@@ -1,33 +1,67 @@
 package rocks.friedrich.jwinf.engine.scenes;
 
-import rocks.friedrich.jwinf.engine.Color;
-
 import ea.Scene;
-import ea.actor.Text;
 import ea.actor.Rectangle;
+import ea.actor.Text;
+import ea.internal.io.FontLoader;
+import rocks.friedrich.jwinf.engine.Color;
 import rocks.friedrich.jwinf.engine.Controller;
 import rocks.friedrich.jwinf.engine.State;
 
 public class MainMenu extends Scene {
 
-  private int y;
+  private final float FONT_SIZE = 0.8f;
 
-  private int x = -10;
+  private final Color AREA_COLOR = new Color("#99d422");
+
+  private final float BORDER_RADIUS = 0.3f;
+
+  /**
+   * aktuelle y-Position.
+   */
+  private float y = 8;
+
+  /**
+   * aktuelle x-Position.
+   */
+  private float x = -10;
+
+  class ColoredArea {
+    private Rectangle rectangle;
+    private String content;
+    private Text text;
+
+    public ColoredArea(String content, float x, float y) {
+      this.content = content;
+      rectangle = createRectangle();
+      text = createText(content);
+      rectangle.setPosition(x - 1, y - FONT_SIZE / 2);
+      text.setPosition(x, y);
+      add(rectangle, text);
+    }
+
+    private Rectangle createRectangle() {
+      Rectangle rectangle = new Rectangle(20, FONT_SIZE * 2);
+      rectangle.setBorderRadius(BORDER_RADIUS);
+      rectangle.setColor(AREA_COLOR);
+      rectangle.addMouseClickListener((vector, mouseButton) -> {
+        System.out.println(content);
+      });
+      return rectangle;
+    }
+
+    private Text createText(String content) {
+      Text text = new Text(content, FONT_SIZE);
+      text.setFont(FontLoader.loadFromFile("fonts/titilium/TitilliumWeb-Bold.ttf"));
+      text.setColor(Color.BLACK);
+      return text;
+    }
+  }
 
   public MainMenu() {
     State.menu.getMain().forEach((main, submenu) -> {
-      Rectangle rectangle = new Rectangle(20, 2);
-      rectangle.setBorderRadius(0.3f);
-      rectangle.setPosition(x - 1, y - 0.5f);
-      rectangle.setColor(new Color("#99d422"));
-      add(rectangle);
-
-      Text text = new Text(main, 1);
-      text.setColor(Color.BLACK);
-      text.setPosition(x, y);
-      add(text);
-
-      y -= 3;
+      new ColoredArea(main, x, y);
+      y -= 2.5 * FONT_SIZE;
     });
   }
 
