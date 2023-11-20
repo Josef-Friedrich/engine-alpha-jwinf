ec905e0a5b69ecd3efbc20eceb8f561e2f5a9f0c
Josef Friedrich <josef@friedrich.rocks>
2023-11-20 21:04:57 +0100

    New folder struture for the tasks

---
+++ src/main/java/rocks/friedrich/jwinf/de/trainingsaufgaben/bedingte_anweisungen_uebungen/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Bedingte Anweisungen – Übungen
+ */
+package rocks.friedrich.jwinf.de.trainingsaufgaben.bedingte_anweisungen_uebungen;
---
+++ src/main/java/rocks/friedrich/jwinf/de/trainingsaufgaben/bedingte_anweisungen_uebungen/zuende_alle_kerzen_an/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Zünde alle Kerzen an
+ */
+package rocks.friedrich.jwinf.de.trainingsaufgaben.bedingte_anweisungen_uebungen.zuende_alle_kerzen_an;
