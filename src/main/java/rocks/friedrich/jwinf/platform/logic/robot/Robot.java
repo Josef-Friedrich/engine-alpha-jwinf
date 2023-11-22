17dfe68df04fe8f97c7f4110815a335443b37b98
Josef Friedrich <josef@friedrich.rocks>
2023-11-22 12:22:48 +0100

    Outsource docs to interface

--- src/main/java/rocks/friedrich/jwinf/platform/logic/robot/Robot.java
+++ src/main/java/rocks/friedrich/jwinf/platform/logic/robot/Robot.java
@@ -6,21 +6,37 @@ public interface Robot {
 
   /**
    * Gehe nach rechts in Richtung Osten.
+   *
+   * @see <a href=
+   *      "https://github.com/France-ioi/bebras-modules/blob/ec1baf055c7f1c383ce8dfa5d27998463ef5be59/pemFioi/blocklyRobot_lib-1.1.js#L3346-L3358">blocklyRobot_lib-1.1.js
+   *      L3346-L3358</a>
    */
   public Movement east();
 
   /**
    * Gehe nach oben in Richtung Norden.
+   *
+   * @see <a href=
+   *      "https://github.com/France-ioi/bebras-modules/blob/ec1baf055c7f1c383ce8dfa5d27998463ef5be59/pemFioi/blocklyRobot_lib-1.1.js#L3318-L3330">blocklyRobot_lib-1.1.js
+   *      L3318-L3330</a>
    */
   public Movement north();
 
   /**
    * Gehe nach links in Richtung Westen.
+   *
+   * @see <a href=
+   *      "https://github.com/France-ioi/bebras-modules/blob/ec1baf055c7f1c383ce8dfa5d27998463ef5be59/pemFioi/blocklyRobot_lib-1.1.js#L3360-L3372">blocklyRobot_lib-1.1.js
+   *      L3360-L3372</a>
    */
   public Movement west();
 
   /**
    * Gehe nach unten in Richtung Süden.
+   *
+   * @see <a href=
+   *      "https://github.com/France-ioi/bebras-modules/blob/ec1baf055c7f1c383ce8dfa5d27998463ef5be59/pemFioi/blocklyRobot_lib-1.1.js#L3332-L3344">blocklyRobot_lib-1.1.js
+   *      L3332-L3344</a>
    */
   public Movement south();
 
--- src/main/java/rocks/friedrich/jwinf/platform/logic/robot/VirtualRobot.java
+++ src/main/java/rocks/friedrich/jwinf/platform/logic/robot/VirtualRobot.java
@@ -158,11 +158,6 @@ public class VirtualRobot implements Robot {
     return new Movement(row, col, dir, movementSuccessful);
   }
 
-  /**
-   * @see <a href=
-   *      "https://github.com/France-ioi/bebras-modules/blob/ec1baf055c7f1c383ce8dfa5d27998463ef5be59/pemFioi/blocklyRobot_lib-1.1.js#L3346-L3358">blocklyRobot_lib-1.1.js
-   *      L3346-L3358</a>
-   */
   public Movement east() {
     if (tryToBeOn(EAST)) {
       col++;
@@ -172,11 +167,6 @@ public class VirtualRobot implements Robot {
     return reportMovement();
   }
 
-  /**
-   * @see <a href=
-   *      "https://github.com/France-ioi/bebras-modules/blob/ec1baf055c7f1c383ce8dfa5d27998463ef5be59/pemFioi/blocklyRobot_lib-1.1.js#L3318-L3330">blocklyRobot_lib-1.1.js
-   *      L3318-L3330</a>
-   */
   public Movement north() {
     if (tryToBeOn(NORTH)) {
       row--;
@@ -186,11 +176,6 @@ public class VirtualRobot implements Robot {
     return reportMovement();
   }
 
-  /**
-   * @see <a href=
-   *      "https://github.com/France-ioi/bebras-modules/blob/ec1baf055c7f1c383ce8dfa5d27998463ef5be59/pemFioi/blocklyRobot_lib-1.1.js#L3360-L3372">blocklyRobot_lib-1.1.js
-   *      L3360-L3372</a>
-   */
   public Movement west() {
     if (tryToBeOn(WEST)) {
       col--;
@@ -200,11 +185,6 @@ public class VirtualRobot implements Robot {
     return reportMovement();
   }
 
-  /**
-   * @see <a href=
-   *      "https://github.com/France-ioi/bebras-modules/blob/ec1baf055c7f1c383ce8dfa5d27998463ef5be59/pemFioi/blocklyRobot_lib-1.1.js#L3332-L3344">blocklyRobot_lib-1.1.js
-   *      L3332-L3344</a>
-   */
   public Movement south() {
     if (tryToBeOn(SOUTH)) {
       row++;
