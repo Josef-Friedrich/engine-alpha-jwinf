78b6baf4030ae4b9b204a8e362ee6f505e1431df
Josef Friedrich <josef@friedrich.rocks>
2023-11-22 09:25:40 +0100

    Add some types

--- javascript-origin/global.d.ts
+++ javascript-origin/global.d.ts
@@ -1,51 +1,53 @@
-declare const modulesPath: string | undefined
+declare const modulesPath: string | undefined;
 
-declare function quickAlgoContext(display: any, infos: any): any
+declare const strings: { messages: Messages };
+
+declare function quickAlgoContext(display: any, infos: any): any;
 
 interface Window {
-  modulesPath?: string
+  modulesPath?: string;
   languageStrings: {
-    messages: Messages
-  }
+    messages: Messages;
+  };
 
-  quickAlgoResponsive: boolean
+  quickAlgoResponsive: boolean;
 }
 
 interface Messages {
-  leavesGrid: string
-  obstacle: string
-  nothingToPickUp: string
-  nothingToLookAt: string
-  falls: string
-  willFallAndCrash: string
-  jumpOutsideGrid: string
-  jumpObstacleBlocking: string
-  jumpNoPlatform: string
-  tooManyObjects: string
-  emptyBag: string
-  successReachExit: string
-  failureReachExit: string
-  successPickedAllWithdrawables: string
-  failurePickedAllWithdrawables: string
-  successContainersFilled: string
-  failureContainersFilled: string
-  failureContainersFilledLess: string
-  failureContainersFilledBag: string
-  failureUnfilteredObject: string
-  failureTooManyMoves: string
-  failureWriteHere: string
-  failureReadHere: string
-  successNumbersWritten: string
-  failureNumbersWritten: string
-  failureNothingToPush: string
-  failureWhilePushing: string
-  failureDropObject: string
-  failureDropPlatform: string
-  failureDropOutside: string
-  failureNotEnoughPlatform: string
-  failureLights: string
-  successLights: string
-  failureLaser: string
-  successPlugsWired: string
-  failurePlugsWired: string
+  leavesGrid: string;
+  obstacle: string;
+  nothingToPickUp: string;
+  nothingToLookAt: string;
+  falls: string;
+  willFallAndCrash: string;
+  jumpOutsideGrid: string;
+  jumpObstacleBlocking: string;
+  jumpNoPlatform: string;
+  tooManyObjects: string;
+  emptyBag: string;
+  successReachExit: string;
+  failureReachExit: string;
+  successPickedAllWithdrawables: string;
+  failurePickedAllWithdrawables: string;
+  successContainersFilled: string;
+  failureContainersFilled: string;
+  failureContainersFilledLess: string;
+  failureContainersFilledBag: string;
+  failureUnfilteredObject: string;
+  failureTooManyMoves: string;
+  failureWriteHere: string;
+  failureReadHere: string;
+  successNumbersWritten: string;
+  failureNumbersWritten: string;
+  failureNothingToPush: string;
+  failureWhilePushing: string;
+  failureDropObject: string;
+  failureDropPlatform: string;
+  failureDropOutside: string;
+  failureNotEnoughPlatform: string;
+  failureLights: string;
+  successLights: string;
+  failureLaser: string;
+  successPlugsWired: string;
+  failurePlugsWired: string;
 }
