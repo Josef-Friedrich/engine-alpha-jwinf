6e61ed6e2bebbbcd91e557d8f3d709bb9257e8f5
Josef Friedrich <josef@friedrich.rocks>
2023-12-10 17:05:37 +0100

    Implement filter

--- src/main/java/rocks/friedrich/jwinf/blockly_robot/logic/level/LevelCollection.java
+++ src/main/java/rocks/friedrich/jwinf/blockly_robot/logic/level/LevelCollection.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import rocks.friedrich.jwinf.blockly_robot.data.model.LevelCollectionData;
 import rocks.friedrich.jwinf.blockly_robot.data.model.LevelData;
@@ -92,7 +94,7 @@ public class LevelCollection
      *
      * @param difficulty the difficulty of the level
      * @param testIndex  the test index of the level (0 is the first test)
-     * 
+     *
      * @return the level corresponding to the given difficulty and test index
      */
     public Level getLevel(Difficulty difficulty, int testIndex)
@@ -104,7 +106,7 @@ public class LevelCollection
      * Retrieves the level for the specified difficulty.
      *
      * @param difficulty the difficulty of the level to retrieve
-     * 
+     *
      * @return the level for the specified difficulty
      */
     public Level getLevel(Difficulty difficulty)
@@ -116,11 +118,32 @@ public class LevelCollection
      * Retrieves the level with the specified difficulty.
      *
      * @param difficulty the difficulty level of the level to retrieve
-     * 
+     *
      * @return the level with the specified difficulty
      */
     public Level getLevel(int difficulty)
     {
         return getLevel(Difficulty.indexOf(difficulty), 0);
+    }
+
+    public Map<Difficulty, List<Level>> filter(Predicate<Difficulty> difficulty,
+            Predicate<Level> level)
+    {
+        return levels.entrySet().stream()
+                .filter((entry) -> difficulty.test(entry.getKey()))
+                .collect(Collectors.toMap(map -> map.getKey(), map -> {
+                    return map.getValue().stream().filter(l -> level.test(l))
+                            .collect(Collectors.toList());
+                }));
+    }
+
+    public Map<Difficulty, List<Level>> filter(Predicate<Difficulty> difficulty)
+    {
+        return filter(difficulty, level -> true);
+    }
+
+    public Map<Difficulty, List<Level>> filter()
+    {
+        return filter(difficulty -> true);
     }
 }
--- src/test/java/rocks/friedrich/jwinf/blockly_robot/logic/level/LevelCollectionTest.java
+++ src/test/java/rocks/friedrich/jwinf/blockly_robot/logic/level/LevelCollectionTest.java
@@ -2,6 +2,7 @@ package rocks.friedrich.jwinf.blockly_robot.logic.level;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static rocks.friedrich.jwinf.TestHelper.loadTask;
+import static rocks.friedrich.jwinf.blockly_robot.logic.level.Difficulty.EASY;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -69,6 +70,33 @@ class LevelCollectionTest
         void testDifficultyAsInt()
         {
             assertEquals(levels.getLevel(0).getTestIndex(), 0);
+        }
+    }
+
+    @Nested
+    class FilterTest
+    {
+        @Test
+        void testFilterByDifficultyAndTestIndex()
+        {
+            var map = levels.filter(difficulty -> difficulty == EASY,
+                    level -> level.getTestIndex() == 0);
+            assertEquals(map.size(), 1);
+            var level = map.get(EASY).get(0);
+            assertEquals(level.getBorderColor(), "#b4ccc7");
+        }
+
+        @Test
+        void testFilterByDifficulty()
+        {
+            var list = levels.filter(difficulty -> difficulty == EASY);
+            assertEquals(list.size(), 1);
+        }
+
+        @Test
+        void testFilter()
+        {
+            assertEquals(levels.filter().size(), 3);
         }
     }
 }
